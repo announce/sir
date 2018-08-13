86dbde108895386652dffa9750fafe2b7aec9f15
Kenta Yamamoto <lq33xxx@gmail.com>
2018-08-13 22:44:08 +0900

    WIP: env, eval

--- src/scm/env.rs
+++ src/scm/env.rs
@@ -1,25 +1,38 @@
-use super::parser::{Atom, NodeElem, SyntaxTree};
+#![feature(box_syntax, box_patterns)]
+use super::parser::{Atom, SyntaxTree};
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq)]
+pub enum Exp {
+    Int(i32),
+    Float(f64),
+    Ops(Fn),
+    Node(SyntaxTree),
+}
+
 pub struct Env {
-    outer: Box<Env>,
-    scope: HashMap<String, SyntaxTree>,
+    outer: Option<Box<Env>>,
+    scope: HashMap<String, Exp>,
 }
 
 impl Env {
-    pub fn find(&self, key: String) {
+    pub fn find(&self, key: &str) -> Exp {
         match self.scope.get(key) {
             Some(val) => val,
-            None => self.outer.find(key),
+            None => match self.outer {
+                Some(o) => o.find(&key),
+                None => panic!("The variable was not found by the key {}", &key),
+            },
         }
     }
 
-    pub fn evaluate(&mut self, tree: SyntaxTree) {
+    pub fn evaluate(&mut self, tree: SyntaxTree) -> Exp {
         match tree {
             SyntaxTree::Leaf(Atom::Symbol(s)) => self.find(s),
-            SyntaxTree::Leaf(cons) => cons,
+            SyntaxTree::Leaf(Atom::Float(f)) => Some(Exp::Float(f)),
+            SyntaxTree::Leaf(Atom::Int(i)) => Exp::Int(i),
             SyntaxTree::Node(n) => match n.first() {
-                NodeElem(Atom::Symbol(ref s)) if s == "quote" => {}
+                (box SyntaxTree::Leaf(Atom::Symbol(ref s))) if s == "quote" => &n[1..],
                 //                NodeElem(SyntaxTree::Leaf(Atom::Symbol(ref s))) if s == "if" => {},
                 //                NodeElem(SyntaxTree::Leaf(Atom::Symbol(ref s))) if s == "set!" => {},
                 //                NodeElem(SyntaxTree::Leaf(Atom::Symbol(ref s))) if s == "define" => {},
